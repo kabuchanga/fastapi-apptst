e55ae64ba5d5718047008a493dab3445251a77d6
kabuchanga <kabuchanga@users.noreply.github.com>
2022-07-30 12:07:02 +0300

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Gunicorn with Uvicorn config to launch in Digital Ocean's App Platform.
+"""
+bind = "0.0.0.0:80"
+workers = 2
+# Uvicorn's Gunicorn worker class
+worker_class = "uvicorn.workers.UvicornWorker"
