13ae240af961ec2580c4e56c0adcf414512eb1c1
kabuchanga <kabuchanga@users.noreply.github.com>
2022-07-30 11:11:41 +0300

    Update entrypointcmd.sh

--- entrypointcmd.sh
+++ entrypointcmd.sh
@@ -2,4 +2,6 @@
 
 RUN_PORT=${PORT:-80}
 
-/usr/local/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker main:app --bind "0.0.0.0:${RUN_PORT}"
+#/usr/local/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker main:app --bind "0.0.0.0:${RUN_PORT}"
+
+uvicorn main:app --proxy-headers --port 80
