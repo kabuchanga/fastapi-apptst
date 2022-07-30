98957118c2f6632373bc96ada2685c221b671432
kabuchanga <kabuchanga@users.noreply.github.com>
2022-07-30 12:26:04 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,8 @@
 """
 Gunicorn with Uvicorn config to launch in Digital Ocean's App Platform.
 """
-bind = "0.0.0.0:80"
+bind = "0.0.0.0:8000"
 workers = 2
 # Uvicorn's Gunicorn worker class
 worker_class = "uvicorn.workers.UvicornWorker"
+pythonpath = "./src"
